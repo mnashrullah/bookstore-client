60f6f9096c2c4fb357a08b0b415e1fcb9b34e8d6
mnashrullah <muhammadnashrullah.it@gmail.com>
2020-02-14 16:10:36 +0700

    make it better again

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,2 +1,2 @@
-// export const mainUrl = 'https://hidden-earth-26826.herokuapp.com';
-export const mainUrl = 'http://localhost:3000';
+export const mainUrl = 'https://hidden-earth-26826.herokuapp.com';
+// export const mainUrl = 'http://localhost:3000';
